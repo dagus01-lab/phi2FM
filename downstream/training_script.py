71a8283c59733f2cc55de240c02da6eae96cee41
carloscca10 <carlos.ccag10@gmail.com>
2025-03-10 17:33:50 +0000

    na

--- downstream/training_script.py
+++ downstream/training_script.py
@@ -920,17 +920,16 @@ if __name__ == "__main__":
 
     # 2. Run main function
     if True:
-        # n_shot_list = [0, 50]
-        n_shot_list = [0, 50, 100, 500, 1000]
+        n_shot_list = [0]
+        # n_shot_list = [0, 50, 100, 500, 1000]
         for n_shot in n_shot_list:
             args.n_shot = n_shot
             for freeze_pretrained in [True, False]:
                 args.freeze_pretrained = freeze_pretrained
                 if n_shot == 0 and not freeze_pretrained:
                     continue
-                # for downstream_task in ['']:
-                # for downstream_task in ['_classification']:
-                for downstream_task in ['lc', 'lc_classification', 'building']:
+                for downstream_task in ['lc']:
+                # for downstream_task in ['lc', 'lc_classification', 'building']:
                     args.downstream_task = downstream_task
                     args.output_channels = 1 if 'building' in args.downstream_task else 11
                     args.model_name = args.model_name + '_classifier' if 'classification' in args.downstream_task else args.model_name
