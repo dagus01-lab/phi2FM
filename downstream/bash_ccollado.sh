5f06c0414da1eaac39c9e055af021b5d80fd1599
carloscca10 <carlos.ccag10@gmail.com>
2025-04-02 22:06:57 +0000

    5000 start

--- downstream/bash_ccollado.sh
+++ downstream/bash_ccollado.sh
@@ -2,14 +2,14 @@
 
 # Define the list of configuration files
 configs=(
-    # "args/ccollado/geoaware.yml"
-    # "args/ccollado/prithvi.yml"
-    # "args/ccollado/satmae.yml"
-    # "args/ccollado/moco.yml"
-    # "args/ccollado/dino.yml"
+    "args/ccollado/geoaware.yml"
+    "args/ccollado/prithvi.yml"
+    "args/ccollado/satmae.yml"
+    "args/ccollado/moco.yml"
+    "args/ccollado/dino.yml"
     "args/ccollado/seco.yml"
     "args/ccollado/uniphi.yml"
-    "args/ccollado/vit.yml"
+    # "args/ccollado/vit.yml"
 )
 
 # Loop through each config file and execute the training script sequentially
